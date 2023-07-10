5649072d2fb5ce437f3a2ec2873128a6f7302cc8
luanpramos <luanpramos@gmail.com>
2023-07-10 18:32:03 -0300

    Firebase login implemented

---
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -0,0 +1,35 @@
+package com.udacity.project4.locationreminders
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.firebase.ui.auth.AuthUI
+import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityRemindersBinding
+
+/**
+ * The RemindersActivity that holds the reminders fragments
+ */
+class RemindersActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRemindersBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRemindersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                (binding.navHostFragment as NavHostFragment).navController.popBackStack()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
